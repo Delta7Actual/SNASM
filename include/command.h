fe5e6ea78f43f3b58aaff5b273525820b6c93a4d
Delta7Actual <142044822+Delta7Actual@users.noreply.github.com>
2025-03-07 19:10:25 +0200

    Command structure update

--- include/command.h
+++ include/command.h
@@ -6,30 +6,35 @@
 #include "definitions.h"
 
 typedef struct s_command {
-    const char name[5];
-    unsigned short opcode;
-    unsigned short funct;
-    unsigned short opcount;
+    const char *name;
+    uint8_t ident;
+    uint8_t opcount;
 } Command;
 
-static const size_t commandsSize = 16;
-static const Command commands[] = {
-    {"mov",   0, 0, 2},
-    {"cmp",   1, 0, 2},
-    {"add",   2, 1, 2},
-    {"sub",   2, 2, 2},
-    {"lea",   4, 0, 2},
-    {"clr",   5, 1, 1},
-    {"not",   5, 2, 1},
-    {"inc",   5, 3, 1},
-    {"dec",   5, 4, 1},
-    {"jmp",   9, 1, 1},
-    {"bne",   9, 2, 1},
-    {"jsr",   9, 3, 1},
-    {"red",  12, 0, 1},
-    {"prn",  13, 0, 1},
-    {"rts",  14, 0, 0},
-    {"stop", 15, 0, 0}
+#define COMMAND_COUNT 16
+
+#define S_OF(opcode, funct) (((opcode & 0xF) << 4) | (funct & 0xF))
+#define G_OP(code) ((code >> 4) & 0xF)
+#define G_FT(code) (code & 0xF)
+
+// Command table
+static const Command commands[COMMAND_COUNT] = {
+    {"mov",   S_OF(0,  0), 2},
+    {"cmp",   S_OF(1,  0), 2},
+    {"add",   S_OF(2,  1), 2},
+    {"sub",   S_OF(2,  2), 2},
+    {"lea",   S_OF(4,  0), 2},
+    {"clr",   S_OF(5,  1), 1},
+    {"not",   S_OF(5,  2), 1},
+    {"inc",   S_OF(5,  3), 1},
+    {"dec",   S_OF(5,  4), 1},
+    {"jmp",   S_OF(9,  1), 1},
+    {"bne",   S_OF(9,  2), 1},
+    {"jsr",   S_OF(9,  3), 1},
+    {"red",   S_OF(12, 0), 1},
+    {"prn",   S_OF(13, 0), 1},
+    {"rts",   S_OF(14, 0), 0},
+    {"stop",  S_OF(15, 0), 0}
 };
 
 const Command *FindCommand(char *com_name);
--- src/command.c
+++ src/command.c
@@ -2,10 +2,15 @@
 
 const Command *FindCommand(char *com_name) {
     if (com_name == NULL) return NULL;
-    for (size_t i = 0; i < commandsSize; i++) {
-        if (strncmp(com_name, commands[i].name, strlen(com_name)) == 0) {
-            return &commands[i];
-        }
+    int l = 0;
+    int r = COMMAND_COUNT - 1;
+    while (l <= r) {
+        int mid = (l + r) / 2;
+        int cmp = strcmp(com_name, commands[mid].name);
+        
+        if (cmp == 0) return &commands[mid];
+        else if(cmp < 0) r = mid - 1;
+        else l = mid + 1;
     }
     return NULL;
 }
